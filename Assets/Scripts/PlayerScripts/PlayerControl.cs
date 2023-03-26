2cd660492e08b5d03ea426a69fc7012089f5107b
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-03-26 15:53:46 +0300

    canvas update

--- Assets/Scripts/PlayerScripts/PlayerControl.cs
+++ Assets/Scripts/PlayerScripts/PlayerControl.cs
@@ -28,7 +28,7 @@ public class PlayerControl : MonoBehaviour
     public float jumpBufferLimit = 0.1f;
 
     //double jump variable
-    public static int extraJumpCounter = 1;
+    public int extraJumpCounter = 1;
     
     //wall jump variable
     public float jumpDirection;
